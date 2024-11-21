85dc33ce42ce67f0664dce8097ee42f2023c86a2
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-11-21 17:30:21 -0500

    Clase Coche y método imprimir o toString

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -4,6 +4,8 @@
  */
 package modelo;
 
+import java.text.NumberFormat;
+
 /**
  *
  * @author Usuario
@@ -41,5 +43,89 @@ public class Coche {
         this.anio = anio;
         this.tipoVehiculo = tipoVehiculo;
     }
+    //3.- ENCAPSULAMIENTO
+    //get: obtener, leer o consultar un atributo
+    public String getPlaca(){
+        return placa;
+    }
+    
+    
+    //set:asignar, sobreescribir un valor del atributo
+    public void setPlaca(String placa){
+        this.placa=placa;
+    }
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Persona getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Persona propietario) {
+        this.propietario = propietario;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+    //4.-MÉTODOS DE REGLA DE NEGOCIO
+    
+     //IMPRIMIR O TOSTRING->IMPRIMIR
+    public void imprimir(){
+        System.out.println("DATOS DEL COCHE\n"+
+                "Placa:"+placa+"\n"+
+                "Modelo:"+modelo+"\n"+
+                "Color:"+getColor()+"\n"+
+                "Precio:"+getPrecio()+"\n"+
+                "Propietario:"+getPropietario()+"\n"+
+                "Año de Fabricación:"+getAnio()+"\n"+
+                "Tipo de Vehículo:"+getTipoVehiculo());
+    }
+
+    @Override
+    public String toString() {
+        return "DATOS DEL COCHE\n"+
+                "Placa:"+placa+"\n"+
+                "Modelo:"+modelo+"\n"+
+                "Color:"+getColor()+"\n"+
+                "Precio:"+getPrecio()+"\n"+
+                "Propietario:"+getPropietario()+"\n"+
+                "Año de Fabricación:"+getAnio()+"\n"+
+                "Tipo de Vehículo:"+getTipoVehiculo();}
+    
+    
 }
