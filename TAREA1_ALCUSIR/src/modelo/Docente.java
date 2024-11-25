fb33c10b8e092d5d29e25d71fe2f89e090a4551a
falcuir <maria.alcusir1989@ist17dejulio.edu.ec>
2024-11-25 05:05:40 -0500

    Clase Docente_Metodos_Constructores

--- TAREA1_ALCUSIR/src/modelo/Docente.java
+++ TAREA1_ALCUSIR/src/modelo/Docente.java
@@ -4,11 +4,13 @@
  */
 package modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Dell Core i7
  */
-public class Docente {
+public class Docente extends Persona{
     
      //1. ATRIBUTOS
     private int codDocente;
@@ -18,7 +20,40 @@ public class Docente {
     private double sueldo;
     private boolean seguro;
     
+    //2. CONSTRUCTORES
     
+    //VACIO
+    
+    public Docente(){
+    
+    }
+    
+    // DATOS DE LA SUBCLASE
+
+    public Docente(int codDocente, String tituloAcademico, String especialidad, int experienciaDocente, double sueldo, boolean seguro) {
+        this.codDocente = codDocente;
+        this.tituloAcademico = tituloAcademico;
+        this.especialidad = especialidad;
+        this.experienciaDocente = experienciaDocente;
+        this.sueldo = sueldo;
+        this.seguro = seguro;
+    }
+    // DATOS HIBRIDOS-SUBCLASE Y SUPERCLASE
+
+    public Docente(int codDocente, String tituloAcademico, String especialidad, int experienciaDocente, double sueldo, boolean seguro, String signo, Date fechaNacimiento, String cedula, String nombre, String apellido, String direccion, String Telefono) {
+        super(signo, fechaNacimiento, cedula, nombre, apellido, direccion, Telefono);
+        this.codDocente = codDocente;
+        this.tituloAcademico = tituloAcademico;
+        this.especialidad = especialidad;
+        this.experienciaDocente = experienciaDocente;
+        this.sueldo = sueldo;
+        this.seguro = seguro;
+    }
+    
+    
+    
+
+    }
     
     
     
